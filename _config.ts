dfb90d49b23d6fa45440880f1c70c08a32f242df
Nap Joseph Calub <njncalub@gmail.com>
2025-04-17 18:23:04 +0800

    feat: add initial page

--- _config.ts
+++ _config.ts
@@ -1,9 +1,20 @@
 import lume from "lume/mod.ts";
 import favicon from "lume/plugins/favicon.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
+import typography from "npm:@tailwindcss/typography";
+import postcss from "lume/plugins/postcss.ts";
+import lightningCss from "lume/plugins/lightningcss.ts";
 
 const site = lume();
 
 site.use(favicon());
+site.use(tailwindcss({
+  options: {
+    plugins: [typography],
+  },
+}));
+site.use(postcss());
+site.use(lightningCss());
 
 site.ignore("README.md");
 
