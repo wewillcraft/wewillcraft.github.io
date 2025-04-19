0368445dd8c71e99ba81aa40e6993411c9e66a8c
Nap Joseph Calub <njncalub@gmail.com>
2025-04-19 18:14:57 +0800

    feat: add articles pages

--- _config.ts
+++ _config.ts
@@ -6,12 +6,23 @@ import tailwindcss from "lume/plugins/tailwindcss.ts";
 import typography from "npm:@tailwindcss/typography";
 import postcss from "lume/plugins/postcss.ts";
 import lightningCss from "lume/plugins/lightningcss.ts";
+import prism from "lume/plugins/prism.ts";
+
+import "npm:prismjs@1.29.0/components/prism-bash.js";
+import "npm:prismjs@1.29.0/components/prism-typescript.js";
+import "npm:prismjs@1.29.0/components/prism-yaml.js";
 
 const site = lume();
 
 site.use(favicon());
 site.use(icons());
 site.use(inline());
+site.use(prism({
+  theme: {
+    name: "okaidia",
+    cssFile: "/styles.css",
+  },
+}));
 site.use(tailwindcss({
   options: {
     plugins: [typography],
